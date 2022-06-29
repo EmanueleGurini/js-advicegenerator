63ff5b0f9bcb24ecc80faa0450183bb2b48ad727
EmanueleGurini <emanuele.gurini89@gmail.com>
2022-06-29 13:01:07 +0200

    Add Doc about cache problem solution

--- api/api.js
+++ api/api.js
@@ -5,6 +5,8 @@
  * @returns {Promise} Promise object represents new advice
  */
 export async function getNewAdvice(url) {
+	// Check this documentation to solve cache problems on FireFox Browser
+	// https://developer.mozilla.org/en-US/docs/Web/API/Request/cache
 	let advice = await fetch(url, {
 		cache: "reload",
 	});
